fd40cc66a07802db6a89072743e2c0f7f34ac21d
HerilS <herilsaha06@gmail.com>
2020-08-03 01:05:17 -0700

    added gsap script tag to load animation faster in production build

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,6 +8,7 @@ export default function Layout({ children }) {
     <React.Fragment>
       <Navbar />
       <Helmet>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.2/gsap.min.js" />
         <link
           rel="stylesheet"
           href="https://cdn.rawgit.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css"
