19eb7699502ec92dcb37736539b789c89a47bb44
HerilS <herilsaha06@gmail.com>
2020-08-17 06:48:11 -0700

    reverting changes

--- src/components/home/PanelSeven.tsx
+++ src/components/home/PanelSeven.tsx
@@ -59,7 +59,7 @@ const useStyles = makeStyles((theme: Theme) =>
           borderColor: '#66AAEE',
           borderWidth: '0.2rem',
         },
-        '&:focus fieldset': {
+        '&:hover fieldset': {
           borderColor: '#66AAEE',
           borderWidth: '0.2rem',
         },
